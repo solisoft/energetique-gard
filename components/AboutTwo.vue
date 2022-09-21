a505bddddb1f0b0b0e9f537495d2a35e262a130d
Olivier Bonnaure <o.bonnaure@wizville.fr>
2022-09-21 08:57:41 +0200

    Update home page

--- components/AboutTwo.vue
+++ components/AboutTwo.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <!--About Page Start-->
   <section class="about-page">
     <div class="container">
@@ -24,7 +25,7 @@
           <div class="about-page__left">
             <div class="about-page__img">
               <img
-                src="~/assets/images/backgrounds/IMG_1185.jpeg"
+                src="~/assets/images/20220805_144924.jpeg"
                 alt=""
               />
 
@@ -35,6 +36,75 @@
     </div>
   </section>
   <!--About Page Start-->
+
+  <section class="about-page">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-6">
+          <div class="about-page__left">
+            <div class="about-page__img">
+              <img
+                src="~/assets/images/20220805_151605.jpeg"
+                alt=""
+              />
+
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-6">
+          <div class="about-page__right">
+            <div class="section-title text-left">
+              <h2 class="section-title__title">
+                En pratique
+              </h2>
+            </div>
+            <p class="about-page__right-text">
+              une séance dure une heure environ. Le patricien écoute, examine le corps et propose un traitement énergétique. Il pourra donner des conseils alimentaires et d'hygiène de vie.
+            </p>
+
+            <a href="/en-pratique" class="introduction__btn thm-btn"><i class="fas fa-arrow-circle-right"></i>En savoir plus.</a>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="about-page">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-xl-6">
+          <div class="about-page__right">
+            <div class="section-title text-left">
+              <h2 class="section-title__title">
+                Contact
+              </h2>
+            </div>
+            <p class="about-page__right-text">
+              Ma formation initiale d’ingénieur et dirigeant au service de l’industrie me pousse à vérifier l’efficacité des théories par l’expérience. L’énergétique traditionnelle chinoise, forte d’un recul millénaire, revêt un côté mystérieux et démontre une véritable efficacité. Mon souhait est d’offrir ces techniques peu connues aux personnes qui en ont besoin. » Henri Thomas
+            </p>
+
+            <a href="/contact" class="introduction__btn thm-btn"><i class="fas fa-arrow-circle-right"></i>En savoir plus.</a>
+
+          </div>
+        </div>
+
+        <div class="col-xl-6">
+          <div class="about-page__left">
+            <div class="about-page__img">
+              <img
+                src="~/assets/images/backgrounds/IMG_1185.jpeg"
+                alt=""
+              />
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  </div>
 </template>
 <script>
 export default {};
